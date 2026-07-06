65b3615effc9aa14c5c3cdd0a595e9f3857bde5a
galaktose <wmatifts@gmail.com>
2026-07-06 23:19:19 +0800

    feat[CharacterBase.cpp/.h]: add AttributesSet and EnhancedInputs
    Added functions for player inputted actions (eg: attack, stance switching, aiming, reloading, etc)

--- Source/CombatSystem/GameplayAbilitySystems/Characters/CharacterBase.cpp
+++ Source/CombatSystem/GameplayAbilitySystems/Characters/CharacterBase.cpp
@@ -1,62 +1,66 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "CharacterBase.h"
 #include "Components/CapsuleComponent.h"
-#include "GameFramework/CharacterMovementComponent.h"	
+#include "GameFramework/CharacterMovementComponent.h"
+#include "EnhancedInput/EnhancedInputComponent.h"          // ADDED
+#include "GameplayTagContainer.h"             // ADDED
 
-// Sets default values
 ACharacterBase::ACharacterBase()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	// Add the AbilitySystemComponent to the character
 	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
 	AbilitySystemComponent->SetIsReplicated(false);
 	AbilitySystemComponent->SetReplicationMode(ReplicationMode);
 
-	// Set size for collision capsule
+	CombatAttributeSet = CreateDefaultSubobject<UCombatAttributeSet>(TEXT("CombatAttributeSet")); // ADDED
+
 	GetCapsuleComponent()->InitCapsuleSize(35.0f, 90.0f);
 
-	//Don't rotate when the controller rotates. Let that just affect the camera.
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationYaw = false;
-	bUseControllerRotationRoll = false;	
+	bUseControllerRotationRoll = false;
 
-	//Configure character movement
-	GetCharacterMovement()->bOrientRotationToMovement = true; //Character moves in the direction of input...
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); //...at this rotation rate
-
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);
 	GetCharacterMovement()->JumpZVelocity = 500.f;
 	GetCharacterMovement()->AirControl = 0.35f;
 	GetCharacterMovement()->MaxWalkSpeed = 500.f;
 	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
 	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
-	GetCharacterMovement()->BrakingDecelerationFalling = 1500.f;	
-
-
+	GetCharacterMovement()->BrakingDecelerationFalling = 1500.f;
 }
 
-// Called when the game starts or when spawned
 void ACharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
-// Called every frame
 void ACharacterBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
-// Called to bind functionality to input
 void ACharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (IA_Attack)
+			EIC->BindAction(IA_Attack, ETriggerEvent::Started, this, &ACharacterBase::Input_Attack);
+		if (IA_Aim)
+		{
+			EIC->BindAction(IA_Aim, ETriggerEvent::Started, this, &ACharacterBase::Input_Aim, true);
+			EIC->BindAction(IA_Aim, ETriggerEvent::Completed, this, &ACharacterBase::Input_Aim, false);
+		}
+		if (IA_SwitchStance)
+			EIC->BindAction(IA_SwitchStance, ETriggerEvent::Started, this, &ACharacterBase::Input_ToggleStance);
+		if (IA_Reload)
+			EIC->BindAction(IA_Reload, ETriggerEvent::Started, this, &ACharacterBase::Input_Reload);
+		if (IA_Special)
+			EIC->BindAction(IA_Special, ETriggerEvent::Started, this, &ACharacterBase::Input_Special);
+	}
 }
 
 void ACharacterBase::PossessedBy(AController* NewController)
@@ -80,4 +84,52 @@ void ACharacterBase::OnRep_PlayerState()
 UAbilitySystemComponent* ACharacterBase::GetAbilitySystemComponent() const
 {
 	return AbilitySystemComponent;
+}
+
+// ADDED below
+void ACharacterBase::SetStance(ECombatStance NewStance)
+{
+	CurrentStance = NewStance;
+
+	if (AbilitySystemComponent)
+	{
+		FGameplayTag MeleeTag = FGameplayTag::RequestGameplayTag(FName("Stance.Melee"));
+		FGameplayTag RangedTag = FGameplayTag::RequestGameplayTag(FName("Stance.Ranged"));
+
+		AbilitySystemComponent->RemoveLooseGameplayTag(MeleeTag);
+		AbilitySystemComponent->RemoveLooseGameplayTag(RangedTag);
+
+		AbilitySystemComponent->AddLooseGameplayTag(
+			NewStance == ECombatStance::Melee ? MeleeTag : RangedTag);
+	}
+}
+
+void ACharacterBase::ToggleStance()
+{
+	SetStance(CurrentStance == ECombatStance::Melee ? ECombatStance::Ranged : ECombatStance::Melee);
+}
+
+void ACharacterBase::Input_ToggleStance()
+{
+	ToggleStance();
+}
+
+void ACharacterBase::Input_Attack()
+{
+	// Day 3: activate melee combo or ranged fire ability depending on CurrentStance
+}
+
+void ACharacterBase::Input_Aim(bool bStarted)
+{
+	// Day 3: add/remove State.Aiming loose tag based on bStarted
+}
+
+void ACharacterBase::Input_Reload()
+{
+	// Day 3
+}
+
+void ACharacterBase::Input_Special()
+{
+	// Day 5
 }
--- Source/CombatSystem/GameplayAbilitySystems/Characters/CharacterBase.h
+++ Source/CombatSystem/GameplayAbilitySystems/Characters/CharacterBase.h
@@ -1,44 +1,71 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
-
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "AbilitySystemInterface.h"
 #include "AbilitySystemComponent.h"
+
+#include "CombatAttributeSet.h"        // Added
 #include "CharacterBase.generated.h"
+
+UENUM(BlueprintType)                  // ADDED
+enum class ECombatStance : uint8
+{
+	Melee UMETA(DisplayName = "Melee"),
+	Ranged UMETA(DisplayName = "Ranged")
+};
 
 UCLASS()
 class COMBATSYSTEM_API ACharacterBase : public ACharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
-
 public:
-	// Sets default values for this character's properties
 	ACharacterBase();
 
-	//Ability System Component
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AbilitySystem")
 	class UAbilitySystemComponent* AbilitySystemComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AbilitySystem")   // ADDED
+		class UCombatAttributeSet* CombatAttributeSet;                             // ADDED
+
+	UPROPERTY(BlueprintReadOnly, Category = "Combat")                          // ADDED
+		ECombatStance CurrentStance = ECombatStance::Melee;                       // ADDED
+
+	UFUNCTION(BlueprintCallable, Category = "Combat")                          // ADDED
+		void SetStance(ECombatStance NewStance);                                  // ADDED
+
+	UFUNCTION(BlueprintCallable, Category = "Combat")                          // ADDED
+		void ToggleStance();                                                       // ADDED
+
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AbilitySystem")
 	EGameplayEffectReplicationMode ReplicationMode = EGameplayEffectReplicationMode::Mixed;
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	
 	virtual void PossessedBy(AController* NewController) override;
-	
 	virtual void OnRep_PlayerState() override;
 
-public:	
-	// Called every frame
+public:
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
+protected:                                                                     // ADDED block
+	void Input_Attack();
+	void Input_Aim(bool bStarted);
+	void Input_ToggleStance();
+	void Input_Reload();
+	void Input_Special();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* IA_Attack;
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* IA_Aim;
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* IA_SwitchStance;
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* IA_Reload;
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* IA_Special;
 };
