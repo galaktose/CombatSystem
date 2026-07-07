4f6712bbc763388ad36c274e94eae8ffd0399a5b
galaktose <wmatifts@gmail.com>
2026-07-07 19:34:09 +0800

    feat[framework]: add basic game frameworks

---
+++ Source/CombatSystem/GameplayAbilitySystems/Managers/Combat_GameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "Combat_GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMBATSYSTEM_API UCombat_GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
