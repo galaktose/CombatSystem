7080820fafab6b667be7874c7e0b617aa7cf7115
galaktose <wmatifts@gmail.com>
2026-07-09 20:31:33 +0800

    feat: add targetdummy.cpp/.h
    - reparented bp_dummy from characterbase to targetdummy
    - targetdummy can now receive damage, handle death and respawn itself in a random location within a set radius around the player

---
+++ Source/CombatSystem/GameplayAbilitySystems/Characters/TargetDummy.cpp
@@ -0,0 +1,76 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TargetDummy.h"
+#include "AbilitySystemComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "../Attributes/CombatAttributeSet.h"
+
+ATargetDummy::ATargetDummy()
+{
+}
+
+void ATargetDummy::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SpawnOrigin = GetActorLocation();
+
+	// Dummy isn't Controller-possessed by default, so init ASC here directly
+	if (AbilitySystemComponent)
+	{
+		AbilitySystemComponent->InitAbilityActorInfo(this, this);
+
+		for (TSubclassOf<UGameplayAbility> AbilityClass : AutoAttackAbilities)
+		{
+			if (AbilityClass)
+			{
+				AbilitySystemComponent->GiveAbility(FGameplayAbilitySpec(AbilityClass, 1));
+			}
+		}
+
+		// Bind health change to detect death
+		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
+			UCombatAttributeSet::GetHealthAttribute()).AddUObject(this, &ATargetDummy::OnHealthChanged);
+	}
+
+	GetWorldTimerManager().SetTimer(AttackTimerHandle, this, &ATargetDummy::PerformAutoAttack, AttackInterval, true);
+}
+
+void ATargetDummy::PerformAutoAttack()
+{
+	if (AutoAttackAbilities.Num() == 0 || !AbilitySystemComponent) return;
+
+	int32 RandomIndex = FMath::RandRange(0, AutoAttackAbilities.Num() - 1);
+	AbilitySystemComponent->TryActivateAbilityByClass(AutoAttackAbilities[RandomIndex]);
+}
+
+void ATargetDummy::OnHealthChanged(const FOnAttributeChangeData& Data)
+{
+	if (Data.NewValue <= 0.f)
+	{
+		HandleDeath();
+	}
+}
+
+void ATargetDummy::HandleDeath()
+{
+	// TODO: play death anim/VFX/SFX before respawn (Step 4 below)
+	GetWorldTimerManager().ClearTimer(AttackTimerHandle);
+
+	FTimerHandle RespawnTimer;
+	GetWorldTimerManager().SetTimer(RespawnTimer, this, &ATargetDummy::Respawn, 1.5f, false);
+}
+
+void ATargetDummy::Respawn()
+{
+	FVector RandomOffset = UKismetMathLibrary::RandomPointInBoundingBox(SpawnOrigin, FVector(RespawnRadius, RespawnRadius, 0.f));
+	SetActorLocation(RandomOffset);
+
+	if (CombatAttributeSet)
+	{
+		CombatAttributeSet->SetHealth(CombatAttributeSet->GetMaxHealth());
+	}
+
+	GetWorldTimerManager().SetTimer(AttackTimerHandle, this, &ATargetDummy::PerformAutoAttack, AttackInterval, true);
+}
+
---
+++ Source/CombatSystem/GameplayAbilitySystems/Characters/TargetDummy.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "CoreMinimal.h"
+#include "CharacterBase.h"
+
+#include "AbilitySystemComponent.h"
+#include "GameplayEffectTypes.h"
+#include "TargetDummy.generated.h"
+
+
+UCLASS()
+class COMBATSYSTEM_API ATargetDummy : public ACharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	ATargetDummy();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	TArray<TSubclassOf<class UGameplayAbility>> AutoAttackAbilities;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	float AttackInterval = 5.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Combat")
+	float RespawnRadius = 500.f;
+
+	FTimerHandle AttackTimerHandle;
+	FVector SpawnOrigin;
+
+	void PerformAutoAttack();
+	void HandleDeath();
+	void Respawn();
+
+	void OnHealthChanged(const FOnAttributeChangeData& Data);
+};
